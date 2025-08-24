5133ca74d670596d6cd99049bc1c6e42a96fe294
Tuomo Björk <tuomo.bjork@tbk.fi>
2025-08-24 09:54:30 +0300

    add: build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Prefer bundled pico-sdk
+if [[ -d ./include/pico-sdk ]]; then
+	export PICO_SDK_PATH="$(realpath ./include/pico-sdk)"
+else
+	export PICO_SDK_PATH
+fi
+
+# Is Ninja available
+if command -v ninja > /dev/null 2>&1; then
+	CUSTOM_OPTS="-G Ninja"
+fi
+
+mkdir -p ./build && cd ./build && cmake $CUSTOM_OPTS .. && cmake --build . -j$(nproc)
